8e054f68cd3a8e46f76cbe691dcfa3c75369b7a7
jhein1 <superjamrs323@gmail.com>
2021-09-02 12:06:51 -0700

    Added (untested) region implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,109 @@
-export function makeHello(name: string) {
-	return `Hello from ${name}!`;
+/* eslint-disable roblox-ts/lua-truthiness */
+import { RunService, Workspace } from "@rbxts/services";
+import { isInShape } from "region-funcs";
+
+abstract class Region {
+    /**
+     * @returns A promise that resolves when a player enters the region
+     */
+    abstract enteredRegion(part: BasePart): Promise<void>;
+    /**
+     * @returns A promise that resolves when a player leaves the region
+     */
+    abstract leftRegion(part: BasePart): Promise<void>;
+    /**
+     * Checks if the player/character is inside the region.
+     * @returns True if the player is in the region, false if not
+     */
+    abstract isInRegion(point: Vector3): boolean;
+}
+
+/**
+ * Simple Region that checks if a specified part enters/exists an area-
+ * optimized for multiple part types. Spheres are the most efficient.
+ */
+export class BasePartRegion extends Region {
+    protected part?: Part;
+    constructor(public location: CFrame, public size: Vector3, public shape: Enum.PartType) {
+        super();
+        // eliminate the potential of the size extending beyond the shape for spheres and cylinders
+        switch (shape) {
+            case Enum.PartType.Ball:
+                const ballRadius = math.max(size.X, size.Y, size.Z);
+                this.size = new Vector3(ballRadius, ballRadius, ballRadius);
+                break;
+            case Enum.PartType.Cylinder:
+                const cylinderRadius = math.max(size.X, size.Z);
+                this.size = new Vector3(cylinderRadius, size.Y, cylinderRadius);
+                break;
+        }
+        if (RunService.IsClient()) {
+            const newPart = new Instance("Part");
+            newPart.CFrame = location;
+            newPart.Size = size;
+            newPart.Shape = shape;
+            newPart.Anchored = true;
+            newPart.Transparency = 1;
+            newPart.CanCollide = false;
+            newPart.CanTouch = true;
+            newPart.Parent = undefined;
+            newPart.Name = tostring(this);
+            this.part = newPart;
+        }
+    }
+    static fromPart(part: BasePart) {
+        return new this(part.CFrame, part.Size, part.IsA("Part") ? part.Shape : Enum.PartType.Block);
+    }
+    async enteredRegion(part: BasePart) {
+        if (RunService.IsClient() && this.part) {
+            this.part.Parent = Workspace;
+            let inRegion = this.isInRegion(part.Position);
+            while (!inRegion) inRegion = part.Touched.Wait()[0] === this.part;
+            this.part.Parent = undefined;
+        } else while (this.isInRegion(part.Position)) wait(0.1);
+    }
+    async leftRegion(part: BasePart) {
+        if (RunService.IsClient() && this.part) {
+            this.part.Parent = Workspace;
+            let inRegion = this.isInRegion(part.Position);
+            while (inRegion) inRegion = !(part.TouchEnded.Wait()[0] === this.part);
+            this.part.Parent = undefined;
+        } else while (this.isInRegion(part.Position)) wait(0.1);
+    }
+    isInRegion(point: Vector3) {
+        return isInShape(point, this.location, this.size, this.shape);
+    }
+}
+
+/**
+ * A handy way to group regions of any type
+ */
+export class RegionUnion {
+    constructor(public regions: Region[]) {}
+    /**
+     * @returns A promise that resolves when a player enters a region
+     */
+    async enteredRegion(part: BasePart) {
+        return Promise.race(this.regions.map((region) => region.enteredRegion(part)));
+    }
+    /**
+     * @returns A promise that resolves when a player leaves a region
+     */
+    async leftRegion(part: BasePart) {
+        return Promise.race(this.isInRegions(part.Position).map((region) => region.leftRegion(part)));
+    }
+    /**
+     * Checks if the player/character is inside all regions.
+     * @returns An array of regions (if any) the player is in
+     */
+    isInRegions(point: Vector3) {
+        return this.regions.filter((region) => region.isInRegion(point));
+    }
+    /**
+     * Checks if the player/character is inside a single region.
+     * @returns A region (if any) the player is in
+     */
+    isInRegion(point: Vector3) {
+        return this.regions.find((region) => region.isInRegion(point));
+    }
 }
---
+++ src/region-funcs.ts
@@ -0,0 +1,27 @@
+const shapes = new Map<Enum.PartType, (point: Vector3, position: CFrame, size: Vector3) => boolean>([
+    [
+        Enum.PartType.Ball,
+        (point, position, size) => {
+            return point.sub(position.Position).Magnitude <= size.X;
+        },
+    ],
+    [
+        Enum.PartType.Cylinder,
+        (point, position, size) => {
+            const relPoint = position.PointToObjectSpace(point).div(new Vector3(1, size.X / 2, 1));
+            const radius = size.X;
+            return math.abs(relPoint.X) <= radius && math.abs(relPoint.Z) <= radius && math.abs(relPoint.Y) <= 1;
+        },
+    ],
+    [
+        Enum.PartType.Block,
+        (point, position, size) => {
+            const relPoint = position.PointToObjectSpace(point).div(size.div(new Vector3(2, 2, 2)));
+            return math.abs(relPoint.X) <= 1 && math.abs(relPoint.Y) <= 1 && math.abs(relPoint.Z) <= 1;
+        },
+    ],
+]);
+
+export function isInShape(point: Vector3, position: CFrame, size: Vector3, type: Enum.PartType) {
+    return shapes.get(type)?.(point, position, size) ?? false;
+}
