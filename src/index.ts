8dda84d67b90afcb5313545ff2358bf418c86e16
jhein1 <superjamrs323@gmail.com>
2021-09-08 15:14:34 -0700

    changed 'step' from a property to an argument to be individually provided

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@
 import { isInShape } from "region-funcs";
 
 abstract class Region {
-    public step: () => Promise<unknown> = async () => task.wait(0.1);
     /** Resolves when the part enters the region and rejects if the timeout (if specified) is reached
      * @returns a promise that resolves when the provided part enters the region */
-    abstract enteredRegion(part: BasePart, timeout?: number): Promise<void>;
+    abstract enteredRegion(part: BasePart, timeout?: number, step?: () => Promise<unknown>): Promise<void>;
     /** Resolves when the part leaves the region and rejects if the timeout (if specified) is reached
      * @returns a promise that resolves when the provided part leaves the region */
-    abstract leftRegion(part: BasePart, timeout?: number): Promise<void>;
+    abstract leftRegion(part: BasePart, timeout?: number, step?: () => Promise<unknown>): Promise<void>;
     /** Determines whether or not a point is within the region */
     abstract isInRegion(point: Vector3): boolean;
 }
@@ -18,12 +17,7 @@ abstract class Region {
  * Can be initialized with a step function that is awaited for custom
  * steps (defaults to `wait(0.1)`). */
 export class BasePartRegion extends Region {
-    constructor(
-        public location: CFrame,
-        public size: Vector3,
-        public shape: Enum.PartType,
-        public step: () => Promise<unknown> = async () => task.wait(0.1),
-    ) {
+    constructor(public location: CFrame, public size: Vector3, public shape: Enum.PartType) {
         super();
         // eliminates the potential of the size extending beyond the shape for spheres and cylinders
         switch (shape) {
@@ -41,13 +35,13 @@ export class BasePartRegion extends Region {
     static fromPart(part: BasePart) {
         return new this(part.CFrame, part.Size, part.IsA("Part") ? part.Shape : Enum.PartType.Block);
     }
-    async enteredRegion(part: BasePart, timeout?: number) {
+    async enteredRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
         return new Promise<void>(async (resolve, reject, onCancel) => {
             const start = os.clock();
             let active = true;
             onCancel(() => (active = false));
             while (!this.isInRegion(part.Position)) {
-                await this.step();
+                await step();
                 if (active && timeout && os.clock() - start > timeout) {
                     reject();
                     return;
@@ -56,13 +50,13 @@ export class BasePartRegion extends Region {
             resolve();
         });
     }
-    async leftRegion(part: BasePart, timeout?: number) {
+    async leftRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
         return new Promise<void>(async (resolve, reject, onCancel) => {
             const start = os.clock();
             let active = true;
             onCancel(() => (active = false));
             while (this.isInRegion(part.Position)) {
-                await this.step();
+                await step();
                 if (active && timeout && os.clock() - start > timeout) {
                     reject();
                     return;
@@ -83,13 +77,13 @@ export class RegionUnion extends Region {
     }
     /** Resolves when the part enters the region and rejects if the timeout (if specified) is reached
      * @returns A promise that resolves when a player enters a region */
-    async enteredRegion(part: BasePart, timeout?: number) {
-        return Promise.race(this.regions.map((region) => region.enteredRegion(part, timeout)));
+    async enteredRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
+        return Promise.race(this.regions.map((region) => region.enteredRegion(part, timeout, step)));
     }
     /** Resolves when the part leaves the region and rejects if the timeout (if specified) is reached
      * @returns A promise that resolves when a player leaves a region */
-    async leftRegion(part: BasePart, timeout?: number) {
-        return Promise.race(this.getRegions(part.Position).map((region) => region.leftRegion(part, timeout)));
+    async leftRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
+        return Promise.race(this.getRegions(part.Position).map((region) => region.leftRegion(part, timeout, step)));
     }
     /** Checks if the point is inside any of the regions in the union
      * @returns An array of regions (if any) the player is in */
@@ -108,13 +102,13 @@ export class RegionNegation extends Region {
     constructor(public region: Region, public negation: Region) {
         super();
     }
-    async enteredRegion(part: BasePart, timeout?: number) {
+    async enteredRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
         return new Promise<void>(async (resolve, reject, onCancel) => {
             const start = os.clock();
             let active = true;
             onCancel(() => (active = false));
             while (!this.isInRegion(part.Position)) {
-                await this.region.step();
+                await step();
                 if (active && timeout && os.clock() - start > timeout) {
                     reject();
                     return;
@@ -123,13 +117,13 @@ export class RegionNegation extends Region {
             resolve();
         });
     }
-    async leftRegion(part: BasePart, timeout?: number) {
+    async leftRegion(part: BasePart, timeout?: number, step: () => Promise<unknown> = async () => wait(0.1)) {
         return new Promise<void>(async (resolve, reject, onCancel) => {
             const start = os.clock();
             let active = true;
             onCancel(() => (active = false));
             while (this.isInRegion(part.Position)) {
-                await this.region.step();
+                await step();
                 if (active && timeout && os.clock() - start > timeout) {
                     reject();
                     return;
